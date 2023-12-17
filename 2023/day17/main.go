0859d1336a7fe29693ffd06a09f3764d00d9021e
devkvlt <devkvlt@pm.me>
2023-12-17 14:16:26 +0100

    Add 2023/day17

---
+++ 2023/day17/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+
+	"github.com/devkvlt/aoc/utils"
+)
+
+// https://pkg.go.dev/container/heap#example-package-PriorityQueue
+
+type State struct {
+	heatloss int
+	pos      Pos
+	dir      Pos
+	straight int
+}
+
+type PQ []*State
+
+func (pq PQ) Len() int {
+	return len(pq)
+}
+
+func (pq PQ) Less(i, j int) bool {
+	return pq[i].heatloss < pq[j].heatloss
+}
+
+func (pq PQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PQ) Push(x any) {
+	*pq = append(*pq, x.(*State))
+}
+
+func (pq *PQ) Pop() any {
+	old := *pq
+	n := len(old)
+	state := old[n-1]
+	old[n-1] = nil // avoid memory leak
+	*pq = old[0 : n-1]
+	return state
+}
+
+var grid = utils.ReadLines("input")
+
+var (
+	height = len(grid)
+	width  = len(grid[0])
+)
+
+type Pos struct{ x, y int }
+
+var (
+	none  = Pos{0, 0}
+	up    = Pos{0, -1}
+	down  = Pos{0, 1}
+	left  = Pos{-1, 0}
+	right = Pos{1, 0}
+)
+
+func isValid(p Pos) bool {
+	return p.x >= 0 && p.x < width && p.y >= 0 && p.y < height
+}
+
+func advance(p, dir Pos) Pos {
+	return Pos{p.x + dir.x, p.y + dir.y}
+}
+
+func split(dir Pos) []Pos {
+	if dir == none {
+		return []Pos{right, down}
+	}
+	return []Pos{{dir.y, dir.x}, {-dir.y, -dir.x}}
+}
+
+type Key struct {
+	pos      Pos
+	dir      Pos
+	straight int
+}
+
+func solveWith(minStraight, maxStraight int) {
+	start := Pos{0, 0}
+	end := Pos{width - 1, height - 1}
+
+	seen := map[Key]bool{}
+
+	pq := PQ{}
+	heap.Push(&pq, &State{heatloss: 0, pos: start, dir: none, straight: 0})
+
+	for len(pq) > 0 {
+		s := heap.Pop(&pq).(*State)
+
+		if s.pos == end && s.straight >= minStraight {
+			fmt.Println(s.heatloss)
+			break
+		}
+
+		key := Key{s.pos, s.dir, s.straight}
+
+		if seen[key] {
+			continue
+		}
+
+		seen[key] = true
+
+		if s.straight < maxStraight && s.dir != none {
+			next := advance(s.pos, s.dir)
+			if isValid(next) {
+				heap.Push(&pq, &State{s.heatloss + int(grid[next.y][next.x]) - 48, next, s.dir, s.straight + 1})
+			}
+		}
+
+		if s.straight >= minStraight || s.dir == none {
+			for _, dir := range split(s.dir) {
+				next := advance(s.pos, dir)
+				if isValid(next) {
+					heap.Push(&pq, &State{s.heatloss + int(grid[next.y][next.x]) - 48, next, dir, 1})
+				}
+			}
+		}
+
+	}
+}
+
+func main() {
+	solveWith(1, 3)  // 758
+	solveWith(4, 10) // 892
+}
--- README.md
+++ README.md
@@ -40,3 +40,4 @@
 | 14  | [Parabolic Reflector Dish](https://adventofcode.com/2023/day/14)       | [main.go](./2023/day14/main.go) |
 | 15  | [Lens Library](https://adventofcode.com/2023/day/15)                   | [main.go](./2023/day15/main.go) |
 | 16  | [The Floor Will Be Lava](https://adventofcode.com/2023/day/16)         | [main.go](./2023/day16/main.go) |
+| 17  | [Clumsy Crucible](https://adventofcode.com/2023/day/17)                | [main.go](./2023/day17/main.go) |
